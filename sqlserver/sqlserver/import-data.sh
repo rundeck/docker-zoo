8c70cf8e59075b67638dd80216374952baab0cbc
Charlie Crawford <ccrawford@pagerduty.com>
2024-08-12 12:13:47 -0400

    Fixes SQL Server data import on latest image.

--- sqlserver/sqlserver/import-data.sh
+++ sqlserver/sqlserver/import-data.sh
@@ -2,4 +2,4 @@
 sleep 10s
 
 #run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i setup.sql
+/opt/mssql-tools18/bin/sqlcmd -C -S localhost -U sa -P $SA_PASSWORD -d master -i setup.sql
